a0e48c0274e12ef2e8cc4f43c96a5444ed6da570
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-11-14 22:21:32 +0100

    Add functions to get operand and operator

--- script.js
+++ script.js
@@ -21,3 +21,55 @@ function operate(operator, num1, num2) {
     : operator === "*" ? multiply(num1, num2)
     : divide(num1, num2);
 }
+
+function getOperand (operand) {
+    if (firstOperand === "" && secondOperand === "") {
+        firstOperand = operand;
+        displayValue = firstOperand;
+    }
+    else if (firstOperand !== "" && secondOperand === "" && operatorCount === 0) {
+        firstOperand += operand;
+        displayValue = firstOperand;
+    }
+    else if (firstOperand !== "" && secondOperand === "" && operatorCount >= 1) {
+        secondOperand = operand;
+        displayValue = secondOperand;
+    }
+    else if (secondOperand !== "" && operatorCount === 1) {
+        secondOperand += operand;
+        displayValue = secondOperand;
+    }
+    else if (secondOperand !== "" && operatorCount > 1) {
+        secondOperand += operand;
+        displayValue = secondOperand;
+    }
+}
+
+function getOperator (operator) {
+    operatorCount += 1;
+
+    if (firstOperand === "" && secondOperand === "") {
+        displayValue = "0";
+    }
+    else if (operatorCount === 1 && firstOperand !== "" && secondOperand === "") {
+        operationPair.push(firstOperand);
+        operationPair.push(operator);
+        operationPair.push(firstOperand);
+        
+    }
+    else if (operatorCount > 1 && firstOperand !== "" && secondOperand !== "") {
+        operationPair = [];
+        operationPair.push(firstOperand);
+        operationPair.push(currentOperator);
+        operationPair.push(secondOperand);
+        console.log(operationPair);
+        result = "";
+        result += performOperation(operationPair);
+        console.log(result);
+        displayValue = result;
+        firstOperand = displayValue;
+        console.log(firstOperand);
+        secondOperand = "";
+    }
+    currentOperator = operator;
+}
